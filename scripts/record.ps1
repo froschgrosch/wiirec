8713c1e11c8bb50f110b68406a6be225006c3bea
froschgrosch <65343661+froschgrosch@users.noreply.github.com>
2023-12-13 21:15:58 +0100

    record.ps1: add more games and modes
    - Remove old comments
    - Use system-wide ffmpeg binaries (placed in SysWOW64)

--- scripts/record.ps1
+++ scripts/record.ps1
@@ -1,13 +1,11 @@
-$games = 'Mario Kart Wii', 'New Super Mario Bros Wii', 'Super Mario Galaxy 2', 'Wii: Party', 'Wii: Play Motion'
-$gamesShortened = 'MKWii', 'NSMBW', 'SMG2', 'WiiParty', 'WiiMotion'
+$games = 'Mario Kart Wii', 'New Super Mario Bros Wii', 'Super Mario Galaxy 2', 'Wii: Party', 'Wii: Play Motion', 'Wii: Sports'
+$gamesShortened = 'MKWii', 'NSMBW', 'SMG2', 'WiiParty', 'WiiMotion', 'WiiSports'
 
-$modes = '60fps copy', '30fps copy', '30fps x264 medium crf21'
+$modes = '60fps copy', '30fps copy', '30fps x264 medium crf21', '60fps FFV1 FLAC', '30fps FFV1 FLAC'
 
-$outputPath = 'G:\record' # without backslash!
-
+$outputPath = 'D:\Simon\WiiRec' #'G:\record' # without backslash!
 
 # Select Game
-#Write-Host '0 : Manuell aufnehmen'
 
 $i = 1
 foreach($game in $games){
@@ -19,7 +17,6 @@ do {
     $sel = [int]$(Read-Host -Prompt 'Spiel auswählen')
 } while (-not (1 .. ($games.Length)).Contains($sel))
 $sel -= 1
-#Write-Output $sel ' '
 
 
 # Select Mode
@@ -33,25 +30,31 @@ foreach($mode in $modes){
 do {
     $selMode = [int]$(Read-Host -Prompt 'Modus auswählen')
 } while (-not (1 .. ($modes.Length)).Contains($selMode))
-#Write-Output $selMode ' '
 
 $filename = Get-Date -UFormat '%Y-%m-%d_%H-%M-%S'
 $filename += "-m$selMode-"
 $filename += $gamesShortened[$sel]
 
-
 Write-Output ' ' "Recording now to $filename. Press q to stop." ' '
 switch ($selMode) {
     1 { # 60fps copy
-        ./ffmpeg.exe -hide_banner -y -f dshow -video_size 640x480 -framerate 60 -sample_rate 48k -i video="USB Video":audio="Eingang (Realtek High Definition Audio)" -c copy "$outputPath\$filename.mkv"    
+        ffmpeg -hide_banner -y -f dshow -video_size 640x480 -framerate 60 -sample_rate 48k -i video="USB Video":audio="Eingang (Realtek High Definition Audio)" -c copy "$outputPath\$filename.mkv"    
     }
 
     2 { # 30fps copy
-        ./ffmpeg.exe -hide_banner -y -f dshow -video_size 640x480 -framerate 30 -sample_rate 48k -pixel_format yuyv422 -i video="USB Video":audio="Eingang (Realtek High Definition Audio)" -c copy "$outputPath\$filename.mkv"
+        ffmpeg -hide_banner -y -f dshow -video_size 640x480 -framerate 30 -sample_rate 48k -pixel_format yuyv422 -i video="USB Video":audio="Eingang (Realtek High Definition Audio)" -c copy "$outputPath\$filename.mkv"
     }
     
     3 { # 30fps x264 medium crf21
-        ./ffmpeg.exe -hide_banner -y -f dshow -video_size 640x480 -framerate 30 -sample_rate 48k -pixel_format yuyv422 -i video="USB Video":audio="Eingang (Realtek High Definition Audio)" -c:v libx264 -crf:v 21 -tune:v animation -preset:v medium -c:a libopus -b:a 64k -aspect 16:9 "$outputPath\$filename.mp4"
+        ffmpeg -hide_banner -y -f dshow -video_size 640x480 -framerate 30 -sample_rate 48k -pixel_format yuyv422 -i video="USB Video":audio="Eingang (Realtek High Definition Audio)" -c:v libx264 -crf:v 21 -tune:v animation -preset:v medium -c:a libopus -b:a 64k -aspect 16:9 "$outputPath\$filename.mp4"
+    }
+
+    4 { # 60fps FFV1 FLAC
+        ffmpeg -hide_banner -y -f dshow -video_size 640x480 -framerate 60 -sample_rate 48k -i video="USB Video":audio="Eingang (Realtek High Definition Audio)" -c:v ffv1 -c:a flac "$outputPath\$filename.mkv"    
+    }
+
+    5 { # 30fps FFV1 FLAC
+        ffmpeg -hide_banner -y -f dshow -video_size 640x480 -framerate 30 -sample_rate 48k -pixel_format yuyv422 -i video="USB Video":audio="Eingang (Realtek High Definition Audio)" -c:v ffv1 -c:a flac "$outputPath\$filename.mkv"
     }
 }
 
