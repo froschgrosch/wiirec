1d7a7c3f77b9471c5b3360331753a6d8e477befa
froschgrosch <65343661+froschgrosch@users.noreply.github.com>
2023-12-13 22:07:30 +0100

    Fix cropping in modes 4 and 5

--- scripts/ingest.ps1
+++ scripts/ingest.ps1
@@ -3,6 +3,8 @@ $outputPath = 'D:\Bibliotheken\Videos\Captures\Wii'
 
 $files = Get-ChildItem $inputPath
 
+$crop = '609:456:18:11'
+
 :fileloop foreach ($f in $files) {
     $name = $f.Name
     $newName = $f.Name.Replace('-m','_m').Remove(20,3).Replace('.mkv','.mp4')
@@ -17,10 +19,10 @@ $files = Get-ChildItem $inputPath
         }
 
         'm4' { # 60fps FFV1 FLAC
-             ffmpeg -y -i "$inputPath\$name" -c:v libx264 -tune:v animation -crf:v 21 -preset:v medium -c:a aac -b:a 128k -aspect 16:9 -vf "crop=609:457:18:11" -movflags +faststart ".\ingest_temp\$newName"           
+             ffmpeg -y -i "$inputPath\$name" -c:v libx264 -tune:v animation -crf:v 21 -preset:v medium -c:a aac -b:a 128k -aspect 16:9 -vf "crop=$crop" -movflags +faststart ".\ingest_temp\$newName"           
         }
         'm5' { # 30fps FFV1 FLAC
-             ffmpeg -y -i "$inputPath\$name" -c:v libx264 -tune:v animation -crf:v 20 -preset:v medium -c:a aac -b:a 128k -aspect 16:9 -vf "crop=609:457:18:11" -movflags +faststart ".\ingest_temp\$newName"           
+             ffmpeg -y -i "$inputPath\$name" -c:v libx264 -tune:v animation -crf:v 20 -preset:v medium -c:a aac -b:a 128k -aspect 16:9 -vf "crop=$crop" -movflags +faststart ".\ingest_temp\$newName"           
         }
         
         'm3' {
