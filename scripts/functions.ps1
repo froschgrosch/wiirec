2f917cbc5aad73971cb8915ade3bac7b17c81e5c
froschgrosch <65343661+froschgrosch@users.noreply.github.com>
2024-06-18 22:27:12 +0200

    Add game and mode selection
    - Update mode check
    - Add option to skip checks

--- scripts/functions.ps1
+++ scripts/functions.ps1
@@ -1,5 +1,5 @@
 # WiiRec Rewrite - https://github.com/froschgrosch/wiirec
-function YNquery($prompt){ # Do a yes/no query and return $true/$false 
+function Confirm-YN($prompt){ # Do a yes/no query and return $true/$false 
     do { 
         $msgboxResult = Read-Host "$prompt (y/n) "
     } while(-not @('y','n').Contains($msgboxResult))
@@ -8,18 +8,14 @@ function YNquery($prompt){ # Do a yes/no query and return $true/$false
 }
 
 function Test-Folder ($path) {
-    if (-not (Test-Path $path)){
-        Write-Host "The folder ""$path"" does not exist."
-        pause
-        exit 1
+    if (-not $config.skipTest.folder -and -not (Test-Path $path)){
+        Exit-Error "The folder ""$path"" does not exist."
     } 
 }
 
 function Test-File ($path) {
-    if (-not (Test-Path -PathType leaf $path)){
-        Write-Host "The file ""$path"" does not exist."
-        pause
-        exit 1
+    if (-not $config.skipTest.folder -and-not (Test-Path -PathType leaf $path)){
+        Exit-Error "The file ""$path"" does not exist."
     } 
 }
 
@@ -31,6 +27,46 @@ function Write-Json ($inputObject, $outputPath){
     ConvertTo-Json -InputObject $inputObject | Out-File $outputPath
 }
 
-function Add-ToObject ($inputObject, $name, $value){
+function Add-ToObject ($inputObject, $value, $name) {
     Add-Member -Force -InputObject $inputObject -MemberType NoteProperty -Name $name -Value $value
+}
+
+function Select-FromArray {
+    Param (
+        [Parameter(Mandatory=$true)] $arr,
+        [Parameter(Mandatory=$true)] [String]$prompt,
+        [Parameter(Mandatory=$false)] $validSelections = 0 .. ($arr.Length - 1)
+    )  <# The selection index that is displayed to the user starts at 1, 
+        # while the index returned by this function starts at 0. #>
+
+    $i = 1
+    foreach($element in $arr){
+        if ($validSelections.Contains($i - 1)){
+            Write-Host "$i`: $($element.name)"
+        }
+        $i++   
+    }
+    Write-Host "c: Exit program"
+
+    do {
+        $sel = Read-Host -Prompt $prompt
+        if ($sel -eq 'c') { exit 0 }
+    } while (-not $validSelections.Contains([int]$sel - 1))
+    return $sel - 1
+}
+
+function Exit-Error {
+    Param (
+        [Parameter(Mandatory=$true)]  [String]$text,
+        [Parameter(Mandatory=$false)] [int]$code = 1
+    )
+
+    Write-Host $text
+    #pause
+    exit $code
+}
+
+function Debug-Selection {
+    Write-Output "DEBUG: Game $i_game (""$($games[$i_game].name)"")" 
+    "DEBUG: Mode $i_mode (""$($config.modes.record[$i_mode].name)"")" 
 }
--- scripts/record.ps1
+++ scripts/record.ps1
@@ -1,20 +1,29 @@
 # WiiRec Rewrite - https://github.com/froschgrosch/wiirec
 # === FUNCTIONS ===
-function Write-Info {
-    Write-Json  $recordinfo "$($config.path.record)\\$filename.json"
+function Write-RecordInfo {
+    Write-Json $recordinfo "$($config.path.record)\\$filename.json"
 }
 
-function Test-Mode ($id) {
-    $env:FFREPORT = '' # 'file=C:/Users/Simon/Downloads/wiirectest/rec/ffreport.log:level=32'
+function Add-RecordInfo ($value, $name) {
+    Add-ToObject $recordinfo $value $name
+}
 
-    $arguments = "-hide_banner", "-y", "-t 1" + $config.modes.record[$id].data + "$($config.path.record)\test.mkv"
 
-    $proc = Start-Process -PassThru -Wait -FilePath "ffmpeg" -ArgumentList $arguments
+function Test-RecordingMode ($id) {
+    if ($config.skipTest.mode) { return $true }
+
+    $arguments = '-hide_banner', '-y', '-t 1' + $config.modes.record[$id].data + "$($config.path.record)\test.mkv"
+    $proc = Start-Process -PassThru -Wait -FilePath 'ffmpeg' -ArgumentList $arguments
     
+    if(Test-Path -PathType leaf "$($config.path.record)\test.mkv") {
+        Remove-Item "$($config.path.record)\test.mkv"
+    }
     return $proc.ExitCode -eq 0
 }
  
 # === INITIALIZATION ===
+Write-Output '=== WiiRec Rewrite ==='
+
 . .\functions.ps1
 Test-File .\data\config.json
 $config = Read-Json .\data\config.json
@@ -24,14 +33,37 @@ $games = Read-Json .\data\games.json
 
 Test-Folder $config.path.record
 
-# test modes
+# select game and mode
+$i_game = Select-FromArray $games 'Select game' #(0 .. 3) ##DEBUG##
+$i_mode = -1
 
+if ($games[$i_game].modes.Length -gt 1){
+    Write-Output "There are multiple valid modes availabe for the game ""$($games[$i_game].name)""." 
+    $i_mode = Select-FromArray $config.modes.record 'Select mode' $games[$i_game].modes
+} 
+elseif ($games[$i_game].modes.Length -eq 1){
+    $i_mode = $games[$i_game].modes[0]
+}
+else {
+    Write-Output "No valid modes are availabe for the game ""$($games[$i_game].name)""." 
+    if (Confirm-YN 'Do you want to select a mode manually?') {
+        $i_mode = Select-FromArray 'Select mode'
+    }
+    else {
+        exit 1
+    }
+}
+Debug-Selection
+
+# test mode
+if (-not (Test-RecordingMode $i_mode)) {
+    Exit-Error "The selected mode ""$($config.modes.record[$i_mode].name)"" does not seem to work. Please investigate."
+}
+
+# === RECORDING ===
 
 # set up recording information
 $recordinfo = New-Object -TypeName 'PSObject'
 
 $filename = (Get-Date -UFormat '%Y-%m-%d_%H-%M-%S-') + $games[0].shortName
-Add-ToObject $recordinfo 'filename' $filename
-#echo $filename
-
-Test-Mode 2
+Add-RecordInfo $filename 'filename'
