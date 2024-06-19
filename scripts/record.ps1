5c3337b151717d688c8fea69d930c9e7755bc83c
froschgrosch <65343661+froschgrosch@users.noreply.github.com>
2024-06-19 20:57:51 +0200

    Add more record info
    - Add stats display
    - Add debug game
    
    - Addendum for commit a4b72f0: Add process priority to config file

--- scripts/record.ps1
+++ scripts/record.ps1
@@ -1,7 +1,7 @@
 # WiiRec Rewrite - https://github.com/froschgrosch/wiirec
 # === FUNCTIONS ===
 function Write-RecordInfo {
-    Write-Json $recordinfo ($config.path.record + '\' + $recordinfo.filename + '.json')
+    Write-Json $recordinfo ($config.path.record + '\' + $recordinfo.file.name + '.json')
 }
 
 function Test-RecordingMode ($id) {
@@ -60,21 +60,21 @@ if (-not (Test-RecordingMode $i_mode)) {
 $recordinfo = New-Object -TypeName 'PSObject'
 $starttime = Get-Date
 
-Add-ToObject $recordinfo 'game' $i_game
-Add-ToObject $recordinfo 'mode' $i_mode
-Add-ToObject $recordinfo 'filename' (($starttime | Get-Date -UFormat '%Y-%m-%d_%H-%M-%S_') + $games[$i_game].shortName)
-Add-ToObject $recordinfo 'recorder' $Env:USERNAME
-
-Add-ToObject $recordinfo 'time' (New-Object -TypeName 'PSObject')
-Add-ToObject $recordinfo.time 'start' ($starttime | Get-Date -UFormat '%Y-%m-%d %H-%M-%S')
+Add-ToObject $recordinfo        'game' $i_game
+Add-ToObject $recordinfo        'mode' $i_mode
+Add-ToObject $recordinfo        'recorder' $Env:USERNAME
+Add-ToObject $recordinfo        'file' (New-Object -TypeName 'PSObject')
+Add-ToObject $recordinfo.file   'name' (($starttime | Get-Date -UFormat '%Y-%m-%d_%H-%M-%S_') + $games[$i_game].shortName)
+Add-ToObject $recordinfo        'time' (New-Object -TypeName 'PSObject')
+Add-ToObject $recordinfo.time   'start' ($starttime | Get-Date -UFormat '%Y-%m-%d %H-%M-%S')
 
 Write-RecordInfo
 
 # start recording
 Clear-Host
-Write-Output "Recording $($games[$i_game].name) in $($config.modes.record[$i_mode].name)." "Filename: $filename - Press q to stop." ' '
+Write-Output "Recording $($games[$i_game].name) in $($config.modes.record[$i_mode].name)." "Filename: $($recordinfo.file.name) - Press q to stop." ' '
 
-$filepath = $config.path.record + '\' + $recordinfo.filename + '.' + $config.modes.record[$i_mode].extension
+$filepath = $config.path.record + '\' + $recordinfo.file.name + '.' + $config.modes.record[$i_mode].extension
 $arguments = '-hide_banner', '-y' + $config.modes.record[$i_mode].data + $filepath
 $proc = Start-Process -NoNewWindow -PassThru -FilePath 'ffmpeg' -ArgumentList $arguments
 
@@ -87,4 +87,11 @@ $stoptime = Get-Date
 Add-ToObject $recordinfo.time 'stop' ($stoptime | Get-Date -UFormat '%Y-%m-%d %H-%M-%S')
 Add-ToObject $recordinfo.time 'duration' (($stoptime - $starttime).ToString('hh\:mm\:ss\.ff'))
 
+Add-ToObject $recordinfo.file      'size' (New-Object -TypeName 'PSObject')
+Add-ToObject $recordinfo.file.size 'raw'  (Get-Item $filepath).Length
+
 Write-RecordInfo
+if (-not $config.displayStats) { exit }
+
+Write-Output ' ' '=== Recording Stats ===' "Started at: $($recordinfo.time.start)" "Stopped at: $($recordinfo.time.stop)" "Duration: $($recordinfo.time.duration)"
+Pause
