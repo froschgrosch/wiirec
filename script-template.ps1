37a59a97895c723683747944a5107849f7ed06e9
froschgrosch <65343661+froschgrosch@users.noreply.github.com>
2024-06-22 01:13:46 +0200

    Add script template

--- script-template.ps1
+++ script-template.ps1
@@ -2,3 +2,13 @@
 # WiiRec Rewrite - https://github.com/froschgrosch/wiirec #
 # Licensed under GNU GPLv3. - File:                       #
 ###########################################################
+
+# === FUNCTIONS ===
+# ...
+
+# === INITIALIZATION ===
+Write-Output '=== WiiRec Rewrite ==='
+
+. .\functions.ps1
+$config = Get-Config
+$games = Get-Games
