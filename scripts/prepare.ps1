2b078587a1696686377ce087fc2f2a4c0b4fbc66
froschgrosch <65343661+froschgrosch@users.noreply.github.com>
2024-06-23 16:06:12 +0200

    Create new script files

---
+++ scripts/prepare.ps1
@@ -0,0 +1,14 @@
+###########################################################
+# WiiRec Rewrite - https://github.com/froschgrosch/wiirec #
+# Licensed under GNU GPLv3. - File: prepare.ps1           #
+###########################################################
+
+# === FUNCTIONS ===
+# ...
+
+# === INITIALIZATION ===
+Write-Output '=== WiiRec Rewrite ==='
+
+. .\functions.ps1
+$config = Get-Config
+$games = Get-Games
